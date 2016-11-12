32807a522850efb0cbc382e082639a2b3608844b
kennebel <jac@kennebel.com>
2016-11-12 11:39:03 -0700

    Initial layout

---
+++ src/TrussLogicCore/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VirtualWhirlwind.Truss.Logic
+{
+    public class Class1
+    {
+		public int Id { get; set; }
+
+        public Class1()
+        {
+        }
+    }
+}
