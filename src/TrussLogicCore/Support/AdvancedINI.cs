24856d0601446e405c60b1eb3e19462b6528c432
kennebel <jac@kennebel.com>
2016-11-18 06:54:31 -0700

    Almost ready to process config files

--- src/TrussLogicCore/Support/AdvancedINI.cs
+++ src/TrussLogicCore/Support/AdvancedINI.cs
@@ -7,102 +7,22 @@ namespace VirtualWhirlwind.Truss.Logic.Support
 {
     public interface IAdvancedINI
     {
-        IAdvancedINI Parent { get; set; }
         List<KeyValuePair<string, object>> Values { get; set; }
-        string FilePath { get; set; }
 
-        bool Read();
-        bool Read(string filePath);
-        bool Save();
-        bool Save(string filePath);
-    }
+		void Clear();
+	}
 
-    public class AdvancedINI : IAdvancedINI
+	public class AdvancedINI : IAdvancedINI
     {
         #region Properties
-        public IAdvancedINI Parent { get; set; }
-
         public List<KeyValuePair<string, object>> Values { get; set; }
+		#endregion
 
-        public string FilePath { get; set; }
-        #endregion
-
-        #region Methods
-        public bool Read()
-        {
-            bool Result = false;
-            if (Parent != null)
-            {
-                Result = Parent.Read();
-            }
-            else
-            {
-                // TODO: determine most efficient cross framework file reading method
-                string Contents = null;
-                try
-                {
-                    Contents = File.ReadAllText(FilePath);
-                }
-                catch
-                {
-
-                }
-
-                if (!string.IsNullOrWhiteSpace(Contents))
-                {
-                    var Lines = Contents.Split(new char[] { '\n' });
-                }
-            }
-
-            return Result;
-        }
-
-        public bool Read(string filePath)
-        {
-            bool Result = false;
-            if (Parent != null)
-            {
-                Result = Parent.Read(filePath);
-            }
-            else
-            {
-                if (File.Exists(filePath)) { FilePath = filePath; }
-                if (!string.IsNullOrWhiteSpace(FilePath)) { Read(); }
-            }
-
-            return Result;
-        }
-
-        public bool Save()
-        {
-            bool Result = false;
-            if (Parent != null)
-            {
-                Result = Parent.Save();
-            }
-            else
-            {
-
-            }
-
-            return Result;
-        }
-
-        public bool Save(string filePath)
-        {
-            bool Result = false;
-            if (Parent != null)
-            {
-                Result = Parent.Save(filePath);
-            }
-            else
-            {
-                FilePath = filePath;
-                Save();
-            }
-
-            return Result;
-        }
-        #endregion
-    }
+		#region Methods
+		public void Clear()
+		{
+			Values.Clear();
+		}
+		#endregion
+	}
 }
---
+++ src/TrussLogicCore/Support/AdvancedINIProcessor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualWhirlwind.Truss.Logic.Support
+{
+	public static class AdvancedINIProcessor
+	{
+		#region Methods
+		public static IAdvancedINI Read(string filePath, IAdvancedINI readInto = null)
+		{
+			if (readInto == null)
+			{
+				readInto = new AdvancedINI();
+			}
+			readInto.Clear();
+
+			// TODO: determine most efficient cross framework file reading method
+			string Contents = null;
+			try
+			{
+				Contents = File.ReadAllText(filePath);
+			}
+			catch
+			{
+
+			}
+
+			if (!string.IsNullOrWhiteSpace(Contents))
+			{
+				var Lines = Contents.Split(new char[] { '\n' }).ToList();
+
+			}
+
+			return readInto;
+		}
+
+		public static bool Save(string filePath, IAdvancedINI toSave)
+		{
+			bool Result = false;
+
+			if (toSave != null)
+			{
+				StringBuilder Output = new StringBuilder();
+
+
+				try
+				{
+					File.WriteAllText(filePath, Output.ToString());
+				}
+				catch
+				{
+
+				}
+			}
+
+			return Result;
+		}
+		#endregion
+
+	}
+}
